150f21f28ae209c32073f376030bc2456fa802c5
Jesse Dijkstra <mail@jessedijkstra.nl>
2016-10-31 16:59:11 +0100

    Fix layout

--- src/components/Activity/component.js
+++ src/components/Activity/component.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default ({ activity })=> (
   <p>
-    <span>{activity.actor.mbox}</span> <strong>{activity.verb.display['en-US']}</strong> <span>{activity.object.definition.name['en-US']}</span>
+    <span>{activity.actor.mbox}</span>{' '}
+    <strong>{activity.verb.display['en-US']}</strong>{' '}
+    <span>{activity.object.definition.name['en-US']}</span>
   </p>
 );
