d11dd2171cc5a42078b28fb1ec00fb14e4e2f867
Jesse Dijkstra <mail@jessedijkstra.nl>
2016-12-20 09:55:38 +0100

    Rename sessions to session

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import statements from './statements';
 import users from './users';
-import sessions from './sessions';
+import session from './session';
 
 export default combineReducers({
   statements,
   users,
-  sessions,
+  session,
   routing
 });
--- src/selectors/session.js
+++ src/selectors/session.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect'
-import { prop, is } from 'ramda';
+import { prop, is, defaultTo, pipe } from 'ramda';
 
-export const getSession = prop('session');
+export const getSession = pipe(prop('session'), defaultTo({}));
 export const getUser = createSelector(getSession, prop('user'));
 export const isAuthenticated = createSelector(getUser, is(Object));
