9f59a0d93b29f70f6e02dbd3258b3157ba3ad7c1
Jesse Dijkstra <mail@jessedijkstra.nl>
2017-01-02 16:05:06 +0100

    Add JUnit reporting for CI

--- config/wdio/ci.js
+++ config/wdio/ci.js
@@ -1,3 +1,12 @@
+const path = require('path');
 const config = require('./defaults.js');
+
+config.reporters = ['dot', 'junit'];
+config.reporterOptions = {
+  outputDir: path.join(process.env.CIRCLE_TEST_REPORTS || './', 'cucumber', 'junit.xml'),
+  outputFileFormat: function(opts) { // optional
+      return `results-${opts.cid}.${opts.capabilities}.xml`
+  }
+}
 
 exports.config = config;
